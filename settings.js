c68530856641a07d89c4ba24ddc5585a2200f5f8
mratifofficial <atifsaleemraisani13@gmail.com>
2025-05-13 14:12:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'atif_aisani',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ATIF KHAN RAISANI",
+  botOwner: 'Professor', // ATIF KHAN RAISANI
+  ownerNumber: '923346590760', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
